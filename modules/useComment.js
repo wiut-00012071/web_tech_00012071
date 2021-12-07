883f059941b33e4800786f64b42ea469e9eafd54
00012071 <wiut00012071@gmail.com>
2021-12-08 00:10:45 +0500

    refactored reddit commets script

--- modules/useComment.js
+++ modules/useComment.js
@@ -1,40 +1,38 @@
 import { months } from "./objects.js";
+
+function displayImage(data) {
+    if (!data.image) return "";
+
+    return `<div class="comment__image">
+                <img src=${data.image} alt="" />
+            </div>`;
+}
+
+function displayDate(data) {
+    if (!data.created) return "";
+
+    const created = data.created.slice(0, 10);
+
+    const [year, month, day] = created.split("-");
+
+    return `<p class="comment__time">
+                ${day} ${months[month - 1]} ${year}
+            </p>;`;
+}
+
 export default function useComment(data) {
-    const username = data.username.slice(3);
-    const title = data.name;
-    const text = data.text;
-
-    let image = "";
-    if (data.image) {
-        image = `<div class="comment__image">
-                    <img src=${data.image} alt="" />
-                </div>`;
-    }
-
-    function displayDate(date) {
-        if (date !== null) {
-            const created = date.slice(0, 10);
-            const [year, month, day] = created.split("-");
-
-            return `${day} ${months[month - 1]} ${year}`;
-        } else {
-            return "No date";
-        }
-    }
     return `<div class="comment">
                 <div class="comment__user">
-                    <h4 class="comment__user-name">${username}</h4>
+                    <h4 class="comment__user-name">
+                        ${data.username.slice(3)}
+                    </h4>
                 </div>
                 <div class="comment__content">
-                    ${image}
+                    ${displayImage(data)}
                     <div class="comment__text">
-                        <h5>${title}</h5>
-                        <p>
-                            ${text}
-                        </p>
-                        <p class="comment__time">${displayDate(
-                            data.created
-                        )}</p>
+                        <h5>${data.name}</h5>
+                        <p>${data.text}</p>
+                        ${displayDate(data)}
                     </div>
                 </div>
             </div>
